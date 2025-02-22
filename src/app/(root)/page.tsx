fd85246bf79ca4afed81d3f4b9ec8ea75970325d
ABDUL NAVAS CK <cknavas123@gmail.com>
2025-02-22 17:33:04 +0530

    root route group created and constans added

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+const Homepage = () => {
+  return (
+    <div>
+    <Button>Click me yaar</Button>
+    </div>
+  )
+}
+
+export default Homepage
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,18 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_NAME, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
 
 export const metadata: Metadata = {
-  title: "Insta Buy",
+  title: {
+    template: "%s | Insta Buy",
+    default: APP_NAME,
+  },
   description: "Ecommerce Platform",
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
